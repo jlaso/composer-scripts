219ec9729cd70398049c4a3361bf14cb758821c1
Joseluis Laso <jlaso@joseluislaso.es>
2016-09-20 08:48:10 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # composer-scripts
-Composer Scripts
+
+Composer Scripts is a way to automatize task 
+when composer finish updating or installing.
+
+Let's see an example:
+
+We want to install a theme in the vendor forlder but
+use it in a source Bundle.
+
+We just have to create a yml file with this structure
+
+```
+# app/config/composer-scripts.yml
+scripts:
+    copy:
+        -
+            source: vendor/gurayyarar/AdminBSBMaterialDesign/css
+            dest: src/Acme/AdminBundle/Resources/public
+            method: ln
+        -
+            source: vendor/gurayyarar/AdminBSBMaterialDesign/images
+            dest: src/Acme/AdminBundle/Resources/public
+            method: ln
+        -
+            source: vendor/gurayyarar/AdminBSBMaterialDesign/js
+            dest: src/Acme/AdminBundle/Resources/public
+            method: ln
+```
+
+
+And declare in the composer.json file the next:
+
+```
+{
+	...
+	"require-dev": {
+	    ...
+		"jlaso/composer-scripts": "1.0",
+		...
+	},
+	"scripts": {
+		"post-install-cmd": [
+			...
+			"JLaso\\ComposerScripts\\Runner::execute"
+		],
+		"post-update-cmd": [
+	        ...
+			"JLaso\\ComposerScripts\\Runner::execute"
+		]
+	},
+	...
+	"extra": {
+	    ...
+		"jlaso-composer-scripts": "app/config/composer-scripts.yml",
+		...
+	}
+}
+
+```
+
+when you launch composer install or composer update at the 
+end of the process composer-scripts will create symbolink
+links for the folders declared.
---
+++ src/composer-scripts/Runner.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace JLaso\ComposerScripts;
+
+use Composer\IO\IOInterface;
+use Composer\Script\Event;
+use Symfony\Component\Yaml\Yaml;
+
+class Runner
+{
+    // currently supported operations
+    protected static $supportedTypes = [
+        'copy',
+    ];
+
+    /**
+     * @var string
+     */
+    protected static $rootFolder;
+
+    /**
+     * @param Event $event
+     */
+    public static function execute(Event $event)
+    {
+        self::$rootFolder = dirname($event->getComposer()->getConfig()->get('vendor-dir'));
+        $extras = $event->getComposer()->getPackage()->getExtra();
+        if(!isset($extras['jlaso-composer-scripts'])){
+            return;
+        }
+        $configuration = $extras['jlaso-composer-scripts'];
+        if(!is_file($configuration)){
+            throw new \InvalidArgumentException('The parameter points to '.$configuration.', but the file doesn\'t exist');
+        }
+        $scripts = Yaml::parse(file_get_contents($configuration));
+        if(!isset($scripts['scripts'])){
+            throw new \InvalidArgumentException('The file '.$configuration.', must contain a scripts root key');
+        }
+        $io = $event->getIO();
+
+        foreach($scripts['scripts'] as $scriptType=>$scriptSequence){
+            if(!in_array($scriptType, self::$supportedTypes)){
+                throw new \InvalidArgumentException('script type '.$scriptType.' not supported!');
+            }
+            foreach ($scriptSequence as $scriptStep) {
+                if(self::$scriptType($scriptStep, $io)){
+
+                };
+            }
+        }
+    }
+
+    /**
+     * @param mixed $data
+     * @param IOInterface $io
+     */
+    protected static function copy($data, IOInterface $io)
+    {
+        $source = self::$rootFolder.DIRECTORY_SEPARATOR.self::doPath($data['source']);
+        $dest = self::doPath($data['dest']);
+        if(!is_file($dest)) {
+            $dest .= DIRECTORY_SEPARATOR.basename($source);
+        }
+        $cmd = isset($data['method']) && ('ln' === strtolower($data['method'])) ? 'ln' : 'cp';
+        $io->write(sprintf("\t%s -> <info>%s</info> -> %s", $data['source'], $cmd, $dest));
+        if('cp' != $cmd){
+            file_exists($dest) && unlink($dest);
+            symlink($source, $dest);
+        }else{
+            copy($source, $dest);
+        }
+    }
+
+    /**
+     * @param string $path
+     * @return string
+     */
+    protected static function doPath($path)
+    {
+        // @TODO: point to bundles with @NameBundle instead to use the whole path
+        //if(preg_match("/^@(?<bundle>\w+)Bundle/", $path, $match)){
+        //    $files = glob('src');
+        //    var_dump($files); die;
+        //}
+        return $path;
+    }
+}
